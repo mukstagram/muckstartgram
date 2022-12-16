17339a297a27162c152a88f3894802b795b447bf
ptg0811 <ptg0811@gmail.com>
2022-12-16 14:46:48 +0900

    #13 router 기본 설정 완료

--- frontreact/src/index.js
+++ frontreact/src/index.js
@@ -1,14 +1,16 @@
-// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/config/configStore";
+import { Provider } from "react-redux";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/config/configStore';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+reportWebVitals();
