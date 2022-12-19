d54dbc999bc1b08544ee125cfb9752c5d21eaf57
hj <dhzhd123@naver.com>
2022-12-19 20:10:51 +0900

    pull하려고하는

--- frontreact/src/pages/FoodPost.js
+++ frontreact/src/pages/FoodPost.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const FoodPost = () => {
   const navigate = useNavigate();
   //에러메시지 관련
-  const [timeErrormsg, setTimeErrormsg] = useState('');
-  const [nameErrormsg, setNameErrormsg] = useState('');
-  const [descErrormsg, setDescErrormsg] = useState('');
-  const [imgErrormsg, setImgErrormsg] = useState('');
+  const [timeErrormsg, setTimeErrormsg] = useState("");
+  const [nameErrormsg, setNameErrormsg] = useState("");
+  const [descErrormsg, setDescErrormsg] = useState("");
+  const [imgErrormsg, setImgErrormsg] = useState("");
 
   //식사시간 관련
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState("");
   const timeChangeHandler = (e) => {
     let value = e.target.value;
     setTime(value);
   };
 
   //음식이름 관련
-  const [foodName, setFoodName] = useState('');
+  const [foodName, setFoodName] = useState("");
   const foodNameChangeHandler = (e) => {
     let value = e.target.value;
     setFoodName(value);
   };
 
   //작성내용 관련
-  const [foodDesc, setFoodDesc] = useState('');
+  const [foodDesc, setFoodDesc] = useState("");
   const foodDescChangeHandler = (e) => {
     let value = e.target.value;
     setFoodDesc(value);
   };
 
   //업로드 이미지 관련
-  const [imgFile, setImgFile] = useState('');
+  const [imgFile, setImgFile] = useState("");
   const foodImgChangeHandler = (e) => {
     let value = e.target.files[0];
     setImgFile(value);
@@ -41,27 +41,27 @@ const FoodPost = () => {
   //등록버튼 onClcik함수
   const submitHandler = () => {
     if (!time) {
-      setTimeErrormsg('식사시간을 선택해주세요');
+      setTimeErrormsg("식사시간을 선택해주세요");
     } else {
-      setTimeErrormsg('');
+      setTimeErrormsg("");
     }
 
     if (!foodName) {
-      setNameErrormsg('음식 이름을 입력해주세요');
+      setNameErrormsg("음식 이름을 입력해주세요");
     } else {
-      setNameErrormsg('');
+      setNameErrormsg("");
     }
 
     if (!foodDesc) {
-      setDescErrormsg('내용을 입력해주세요');
+      setDescErrormsg("내용을 입력해주세요");
     } else {
-      setDescErrormsg('');
+      setDescErrormsg("");
     }
 
     if (!imgFile) {
-      setImgErrormsg('사진을 첨부해주세요');
+      setImgErrormsg("사진을 첨부해주세요");
     } else {
-      setImgErrormsg('');
+      setImgErrormsg("");
     }
     //데이터 전달 명령 필요
 
@@ -76,7 +76,7 @@ const FoodPost = () => {
       <WrapWrap>
         <Partition>
           <div>
-            식사 시간{' '}
+            식사 시간{" "}
             <TimeSelector onChange={timeChangeHandler}>
               <option value="">---선택---</option>
               <option value="아침">아침</option>
@@ -88,7 +88,7 @@ const FoodPost = () => {
         </Partition>
         <Partition>
           <div>
-            음식 이름{' '}
+            음식 이름{" "}
             <InputTitleSpace
               type="text"
               maxLength="20"
@@ -100,7 +100,7 @@ const FoodPost = () => {
         </Partition>
         <Partition>
           <div>
-            내용 작성{' '}
+            내용 작성{" "}
             <InputDescSpace
               type="text"
               maxLength="100"
@@ -111,7 +111,7 @@ const FoodPost = () => {
           <ErrorMsg>{descErrormsg}</ErrorMsg>
         </Partition>
         <Partition>
-          업로드 사진{' '}
+          업로드 사진{" "}
           <InputImgSpace
             onChange={foodImgChangeHandler}
             type="file"
