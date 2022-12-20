4ea29326372fa0aeb16b8b0d42eb12a187c6dbaa
kimheeseok <kim1140129@gmail.com>
2022-12-20 20:29:02 +0900

    <Fix> 로그인 인증 미들웨어 추가
    food.route.js,
    comment.route.js
    authUserMiddleware 미들웨어 추가 작업 진행

--- backend/src/routes/comment.route.js
+++ backend/src/routes/comment.route.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const CommentController = require('../workspace/comments/comment.controller');
 const commentController = new CommentController();
+const authUserMiddleware  = require('../middlewares/authUserMiddleware.js');
 
-router.post('/:foodId', commentController.createComment);
+
+router.post('/:foodId',authUserMiddleware, commentController.createComment);
 router.get('/:foodId', commentController.getComments);
-router.patch('/:commentId', commentController.editComment);
-router.delete('/:commentId', commentController.deleteComment);
+router.patch('/:commentId',authUserMiddleware, commentController.editComment);
+router.delete('/:commentId',authUserMiddleware, commentController.deleteComment);
 
 module.exports = router;
--- backend/src/routes/food.route.js
+++ backend/src/routes/food.route.js
@@ -4,10 +4,12 @@ const FoodController = require('../workspace/foods/food.controller.js');
 const foodController = new FoodController();
 const CommentController = require('../workspace/comments/comment.controller.js');
 const commentController = new CommentController();
-const upload = require("../middlewares/awsMiddleware");
+const upload = require("../middlewares/awsMiddleware.js");
+const authUserMiddleware  = require('../middlewares/authUserMiddleware.js');
+
 
 router.get('/',foodController.getFoodList);
-router.post('/',upload.single('thumbnail'),foodController.createdFoodPost);
+router.post('/',authUserMiddleware,upload.single('thumbnail'),foodController.createdFoodPost);
 router.get('/:foodId',foodController.getFoodDetail);
 router.get('/:foodId/comments',commentController.getComments);
 module.exports = router;
