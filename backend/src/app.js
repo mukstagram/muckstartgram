b1af6d62b96df2f314a932800a4875ba38148d89
kimheeseok <kim1140129@gmail.com>
2022-12-20 19:45:56 +0900

    <Feat> Logger File 추가
    Log 추가 WinstonUtil.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,11 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
+const morgan = require('morgan');
+const Winston = require('./util/WinstonUtil.js')
+
+app.use(morgan(":method :status :url :response-time ms", { stream: Winston.stream }));
+
 //Body
 app.use(express.json());
 app.use(cookieParser());
