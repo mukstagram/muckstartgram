ccf6647cf6374b1ee9d15540f33cc877bc49c48b
gPwn <hyeju3014@gmail.com>
2022-12-21 21:23:30 +0900

    #8 <Fix> app.js 수정

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,6 @@ const {
     errorLogger,
 } = require('./middlewares/error-hander.middleware');
 
-const cookieParser = require('cookie-parser');
-
 const app = express();
 const PORT = process.env.PORT;
 const cors = require('cors');
@@ -21,21 +19,12 @@ app.use(
 
 //Body
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors({ exposedHeaders: ['Authorization'] }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
-
-// app.use((req,res,next)=>{
-//     if (req.headers['content-type'] !== 'application/json') {
-//         res.status(403).send('Forbidden');
-//         return;
-//     }
-//     next();
-// })
 
 // 라우터 등록
 app.use('/api', routes);
